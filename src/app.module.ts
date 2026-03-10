2b6df736651bf114393309ef24bee17ac99f5cae
Rodrigo Urrea <rurream@gmail.com>
2026-03-10 09:44:25 -0300

    primer commit nest pokedex

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), renderPath: '/',
+    }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule
+  ],
+  controllers: [],
+  providers: [],
+
+})
+export class AppModule { }
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,103 @@
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { isValidObjectId, Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+  ) { }
+
+  private handleExceptions(error: unknown) {
+    if (typeof error === 'object' && error !== null && 'code' in error && 'keyValue' in error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
+      }
+    } else {
+      throw new InternalServerErrorException(`Can't create pokemon - Check server logs`);
+    }
+  }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase().trim();
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+      return pokemon;
+    } catch (error: unknown) {
+
+      this.handleExceptions(error);
+    }
+
+  }
+
+  async findAll() {
+    return await this.pokemonModel.find();
+  }
+
+  async findOne(term: string) {
+
+    let pokemon: Pokemon | null = null;
+
+    //por no
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: Number(term) });
+    }
+
+    // por MongoID
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    // name
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLocaleLowerCase().trim() });
+    }
+
+    if (!pokemon) throw new NotFoundException(`Pokemon with id, name or no "${term}" not found`)
+
+    return pokemon;
+  }
+
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    let pokemon: Pokemon | null = await this.findOne(term);
+
+    if (updatePokemonDto.name) updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase().trim();
+
+    try {
+
+      // await pokemon.updateOne(updatePokemonDto, { new: true });
+
+      pokemon = await this.pokemonModel.findByIdAndUpdate({ _id: pokemon._id }, updatePokemonDto, { new: true });
+
+      return pokemon;
+
+    } catch (error: unknown) {
+      this.handleExceptions(error);
+    }
+
+
+  }
+
+  async remove(id: string) {
+    // const pokemon = await this.findOne(id);
+
+    // await this.pokemonModel.deleteOne({ _id: pokemon._id });
+    // // await pokemon.deleteOne();
+
+    const result = await this.pokemonModel.findByIdAndDelete(id);
+    if (!result) throw new BadRequestException(`Pokemon with id "${id}" not found`);
+
+  }
+}
